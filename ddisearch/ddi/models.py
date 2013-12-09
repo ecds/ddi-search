6d36d70b3ec46008425deb8bf823890b6d85a80f
rlskoeser <rebecca.s.koeser@emory.edu>
2013-12-09 15:28:29 -0500

    add logic to group dates (single or date ranges) in a record, for easy display [#62179772]

--- ddisearch/ddi/models.py
+++ ddisearch/ddi/models.py
@@ -3,6 +3,11 @@ from eulxml import xmlmap
 from eulexistdb.models import XmlModel
 from eulexistdb.manager import Manager
 
+class TimePeriod(xmlmap.XmlObject):
+    event = xmlmap.StringField('@event')  # start, end, single
+    date = xmlmap.StringField('@date')
+    cycle = xmlmap.StringField('@cycle')
+
 class CodeBook(XmlModel):
     # NOTE: may want to adjust xpaths for effeciency when/if we query on them
     title = xmlmap.StringField('stdyDscr/citation/titlStmt/titl')
@@ -12,9 +17,10 @@ class CodeBook(XmlModel):
     keywords = xmlmap.StringListField('stdyDscr/stdyInfo/subject/keyword')
     topics = xmlmap.StringListField('stdyDscr/stdyInfo/subject/topcClas')
 
-    dates = xmlmap.NodeListField('.//timePrd', xmlmap.XmlObject)
+    time_periods = xmlmap.NodeListField('.//timePrd', TimePeriod)
     # technically probably a datefield; could be 4 digit year, or YYYY-MM
     # full xpath is stdyDscr/stdyInfo/sumDscr
+    # XPath for sorting by date; sort on the earliest date available (single or date range)
     sort_date_xpath = 'min(%(xq_var)s//timePrd/string(@date))'
 
     # boosted fields in the index that should be searched to get a tuned
@@ -22,5 +28,34 @@ class CodeBook(XmlModel):
     boostfields = xmlmap.StringField('.//titl | .//abstract | .//geogCover')
 
     objects = Manager('/codeBook')
+
+    @property
+    def dates(self):
+        '''List of dates or date ranges associated with this dataset.
+
+        Iterates through all the timePrd elements and groups any start/end
+        dates for the same cycle to generate a list of dates or date ranges
+        that can easily be displayed.
+        '''
+        dates = []
+        start = {}
+        for t in self.time_periods:
+            if t.event == 'single':
+                dates.append(t.date)
+            elif t.event == 'start':
+                start[t.cycle] = t.date
+            elif t.event == 'end':
+                if t.cycle in start:
+                    # FIXME: how best to display two dates when date could
+                    # be year or year-month-day ?
+                    sep = '-'
+                    if '-' in start[t.cycle] or '-' in t.date:
+                        sep = ' - '
+                    dates.append('%s%s%s' % (start[t.cycle], sep, t.date))
+
+        return dates
+
+
+
 
 
--- ddisearch/ddi/tests.py
+++ ddisearch/ddi/tests.py
@@ -1,3 +1,43 @@
+import os
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+from eulxml.xmlmap import load_xmlobject_from_file
+
+from ddisearch.ddi.models import CodeBook, TimePeriod
+
+FIXTURE_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+
+class CodeBookTest(TestCase):
+
+    def setUp(self):
+        self.cb = load_xmlobject_from_file(os.path.join(FIXTURE_DIR, '02988.xml'),
+                                           CodeBook)
+
+    def test_properties(self):
+        self.assertEqual('Israeli Election Study, 1973', self.cb.title)
+        self.assert_(self.cb.abstract.startswith('This study is one in a series of election studies conducted since 1969 by Alan Arian'))
+        self.assertEqual(2, len(self.cb.authors))
+        self.assert_('Arian, Asher' in self.cb.authors)
+        self.assert_('Turgovnik, Ephraim' in self.cb.authors)
+        self.assertEqual(35, len(self.cb.keywords))
+        self.assert_('Arab Israeli conflict' in self.cb.keywords)
+        self.assert_('social attitudes' in self.cb.keywords)
+        self.assertEqual(4, len(self.cb.topics))
+        self.assert_('Mass Political Behavior and Attitudes' in self.cb.topics[0])
+        self.assertEqual(5, len(self.cb.time_periods))
+        self.assert_(isinstance(self.cb.time_periods[0], TimePeriod))
+        self.assertEqual('single', self.cb.time_periods[0].event)
+        self.assertEqual('1973', self.cb.time_periods[0].date)
+        self.assertEqual('P1', self.cb.time_periods[0].cycle)
+        self.assertEqual('start', self.cb.time_periods[1].event)
+
+    def test_dates(self):
+        dates = self.cb.dates
+        # one single date and two sets of date ranges
+        self.assertEqual(3, len(dates))
+        self.assertEqual('1973', dates[0])  # single date
+        self.assertEqual('1974-01-13 - 1974-01-15', dates[1])
+        self.assertEqual('1973-12-19 - 1973-12-20', dates[2])
+
