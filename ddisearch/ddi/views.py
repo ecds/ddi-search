0162d0d533e6ea7cb2934dde7ec78877b17ac535
rlskoeser <rebecca.s.koeser@emory.edu>
2014-02-04 11:42:49 -0500

    update search view logic to search on title/summary/source when there are terms present [#60341698]

--- ddisearch/ddi/views.py
+++ ddisearch/ddi/views.py
@@ -15,34 +15,48 @@ def search(request):
     form = forms.KeywordSearch(request.GET)
     context = {'form': form}
 
-    required_terms = ['keyword', 'per_page', 'sort']
-    # if the form is not valid and *none* of the required terms are present,
-    # assume this is a new search and display an empty search form
-    if not form.is_valid() and not any(d in request.GET for d in required_terms):
-        # re-init for display, without any 'required field' error messages
-        context['form'] = forms.KeywordSearch()
-
-    elif form.is_valid():
-        # generate query here
-        keywords = form.cleaned_data['keyword']
+    # if the form is valid, process and generate search results;
+    # if not, fall through and display the form with validation errors
+    if form.is_valid():
+        search_opts = form.cleaned_data
         per_page = form.cleaned_data['per_page']
         sort = form.cleaned_data['sort']
 
+        # build query based on search terms here
+        results = CodeBook.objects.all()
 
-        results = CodeBook.objects \
-                    .filter(fulltext_terms=keywords) \
-                    .or_filter(fulltext_terms=keywords,
-                               boostfields__fulltext_terms=keywords,
-                               highlight=False,    # disable highlighting in search results list
-                               ) \
-                    .order_by(sort) \
-                    .only('title', 'abstract', 'keywords', 'topics',
-                          'authors', 'time_periods', 'fulltext_score')
+        if 'keyword' in search_opts and search_opts['keyword']:
+            keywords = search_opts['keyword']
+            results = results.filter(fulltext_terms=keywords)
+                             # .or_filter(fulltext_terms=keywords,
+                             #            boostfields__fulltext_terms=keywords,
+                             #            highlight=False    # disable highlighting in search results list
+                             #            )
 
-        if sort == 'title':
-            results = results.order_by(sort)
-        elif sort == 'relevance':
-            results = results.order_by('-fulltext_score')
+        if 'title' in search_opts and search_opts['title']:
+            results = results.filter(title__fulltext_terms=search_opts['title'])
+        if 'summary' in search_opts and search_opts['summary']:
+            results = results.filter(abstract__fulltext_terms=search_opts['summary'])
+        if 'source' in search_opts and search_opts['source']:
+            results = results.filter(authors__fulltext_terms=search_opts['source'])
+
+        # To make relevance scores more meaningful, run *all* search terms
+        # from any field against the full text and boost fields
+        results = results.or_filter(fulltext_terms=form.all_search_terms,
+                                    boostfields__fulltext_terms=form.all_search_terms,
+                                    highlight=False    # disable highlighting in search results list
+                                    )
+        # FIXME: redundancy in boost fields seems to generate very high relevance scores
+        # see if we can avoid repeating filters (e.g. search by title then search again in boost fields)
+
+        # only return fields needed to generate search results
+        results = results.only('title', 'abstract', 'keywords', 'topics',
+                               'authors', 'time_periods', 'fulltext_score')
+
+        # simple sort mapping
+        sort_map = {'title': 'title', 'relevance': '-fulltext_score'}
+        if sort in sort_map:
+            results = results.order_by(sort_map[sort])
         elif sort.startswith('date'):
             # either date sorting requires a raw xpath to sort on earlist date
             # check which type to determine if results should be ascending or note
@@ -78,7 +92,8 @@ def search(request):
         del partial_args['sort']
         sort_params = urlencode(partial_args)
 
-        context.update({'keywords': keywords, 'results': results,
+        context.update({'keywords': search_opts.get('keywords', ''),
+            'results': results,
             'pages': pages, 'url_params': url_params,
             'per_page': int(per_page),
             'rechunk_params': rechunk_params,
