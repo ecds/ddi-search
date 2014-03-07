0821f3263e0efd0fbe8e8bb709fdb29ea6fe2cad
rlskoeser <rebecca.s.koeser@emory.edu>
2014-03-07 10:41:48 -0500

    fix 'None' for date fields on pagination

--- ddisearch/ddi/views.py
+++ ddisearch/ddi/views.py
@@ -118,6 +118,11 @@ def search(request):
             results = paginator.page(paginator.num_pages)
 
         url_args = form.cleaned_data
+        # clear out blank fields
+        # (in particular, without this numeric date fields come through as None)
+        for k in list(url_args.keys()):
+            if url_args[k] is None or url_args[k] == '':
+                del url_args[k]
         url_params = urlencode(url_args)
         # url params for changing chunk size
         partial_args = url_args.copy()
