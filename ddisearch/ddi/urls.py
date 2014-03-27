ec1c15755ab26d86ee32147d3b466aff22d4825b
rlskoeser <rebecca.s.koeser@emory.edu>
2014-03-27 16:50:10 -0400

    adjust URL patterns to accommodate non-ICPSR ids

--- ddisearch/ddi/urls.py
+++ ddisearch/ddi/urls.py
@@ -4,9 +4,9 @@ from ddisearch.ddi import views
 
 urlpatterns = patterns('',
     url(r'^search/$', views.search, name='search'),
-    url(r'^resources/(?P<agency>[a-zA-Z]+):(?P<id>[0-9]+)/$',
+    url(r'^resources/(?P<agency>[a-zA-Z0-9]+):(?P<id>[0-9_]+)/$',
         views.resource, name='resource'),
-    url(r'^resources/(?P<agency>[a-zA-Z]+):(?P<id>[0-9]+)/XML/$',
+    url(r'^resources/(?P<agency>[a-zA-Z0-9]+):(?P<id>[0-9_]+)/XML/$',
         views.resource_xml, name='xml'),
     url(r'^(?P<mode>(topics|keywords))/$', views.browse_terms,
         name='browse-terms'),
