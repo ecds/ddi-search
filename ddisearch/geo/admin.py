3ade57d242c4d4d7ffd24bc27a4b95a1ed9ad20b
rlskoeser <rebecca.s.koeser@emory.edu>
2014-03-27 16:38:57 -0400

    revised db models for geographic functionality, with fixture data for geonames countries & continents [#64421978]

--- ddisearch/geo/admin.py
+++ ddisearch/geo/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from ddisearch.geo.models import Location, GeonamesCountry,  GeonamesContinent
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'geonames_id', 'country_code', 'feature_code',
-                    'latitude', 'longitude')
-    list_filter = ('country_code', 'feature_code')
-    search_fields = ('name', 'geonames_id', 'country_code')
+    list_display = ('name', 'geonames_id', 'country_code', 'continent_code',
+                    'feature_code', 'latitude', 'longitude', 'state_code')
+    list_filter = ('country_code', 'continent_code', 'feature_code', 'state_code')
+    search_fields = ('name', 'geonames_id', 'country_code', 'state_code')
 
 
 class GeonamesCountryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'continent')
+    list_display = ('name', 'code', 'continent', 'geonames_id')
     list_filter = ('continent',)
-    search_fields = ('name', 'code', 'continent')
+    search_fields = ('name', 'code', 'continent', 'geonames_id')
 
 
 class GeonamesContinentAdmin(admin.ModelAdmin):
--- ddisearch/geo/models.py
+++ ddisearch/geo/models.py
@@ -4,11 +4,16 @@ from django.db import models
 
 
 class GeonamesCountry(models.Model):
-    'Minimal country information, based on geonames country info download'
+    '''Minimal country information, based on geonames country info download
+    http://download.geonames.org/export/dump/countryInfo.txt'''
+    #: country name
     name = models.CharField(max_length=255)
+    #: two-letter country code
     code = models.CharField(max_length=2, unique=True)
+    #: two-letter continent code
     continent = models.CharField(max_length=2)
-    # TODO: may want foreign key relation to continent instead...
+    #: numeric geonames id
+    geonames_id = models.IntegerField()
 
     class Meta:
         verbose_name_plural = 'geonames countries'
@@ -17,25 +22,39 @@ class GeonamesCountry(models.Model):
         return '%s (%s)' % (self.name, self.code)
 
 class GeonamesContinent(models.Model):
-    'Continent names and codes from GeoNames'
+    '''Continent names and codes from GeoNames, as listed at
+    http://download.geonames.org/export/dump/'''
+    #: continent name
     name = models.CharField(max_length=255)
+    #: two-letter continent code
     code = models.CharField(max_length=2, unique=True)
+    #: geonames id
     geonames_id = models.IntegerField()
 
     def __unicode__(self):
         return '%s (%s)' % (self.name, self.code)
 
 class Location(models.Model):
+    #: name of the location
     name = models.CharField(max_length=255)
+    #: numeric genoames id, entered in the XML as geonames:#####
     geonames_id = models.IntegerField(unique=True)
+    #: latitude
     latitude = models.FloatField()
+    #: longitude
     longitude = models.FloatField()
-    country_code = models.CharField(max_length=10, null=True, blank=True)  # 2 ?
-
-    # type/level of place ?
+    #: 2-character country code
+    country_code = models.CharField(max_length=2, null=True, blank=True)
+    #: type or level of place; see http://www.geonames.org/export/codes.html
     feature_code = models.CharField(max_length=10)
-    # continent code ?
+    #: two-letter continent code
+    continent_code = models.CharField(max_length=10, null=True, blank=True)
+    #: two-digit 'adminCode1' from geonames; corresponds to US state
+    #: codes, and should allow for grouping US cities by state
+    state_code = models.CharField(max_length=3, null=True, blank=True)
 
-    # ?? FIXME; maybe use relations instead of country code ?
-    # geonames_country = models.ForeignKey(GeonamesCountry, to_field='code')
+    def __unicode__(self):
+        return '%s (%s, %s)' % (self.name, self.country_code, self.continent_code )
+
+
 
